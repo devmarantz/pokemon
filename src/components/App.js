61990c88589ceeee4a073970acc5a7a84b8b2d5d
Ivy <yunjilim@gmail.com>
2019-03-12 16:27:06 -0700

    adding to team, displaying pokemon info

--- src/components/App.js
+++ src/components/App.js
@@ -12,20 +12,34 @@ class App extends Component {
       myTeam: []   
     }
   }
-  /**
-   * lifecycle component did mount to setState
-  */
-  // componentDidMount() {
-  //   API.getAllPokemon().then(res => console.log(res.data))
-  // }
+
+  updateTeam = () => {
+    if (this.state.myTeam.length < 6) {
+      let flag = true;
+      this.state.myTeam.forEach(pokemon => {
+        if (pokemon.name === this.state.pokemon.name) {
+          alert('sorry cant add') 
+          flag = false; 
+        } 
+      }) 
+      if (flag) {
+        this.setState(prevState => {
+          const updateTeam = prevState.myTeam; 
+          updateTeam.push(this.state.pokemon);
+          // myTeam: this.state.myTeam.push(this.state.pokemon) 
+        return { myTeam: updateTeam }
+        }) 
+      }
+    } else {
+      // need to remove pokemon
+      alert('Team Full!')
+    }
+  }
 
   // get pokemon information
-
   handleSearchFunction = searchText => {
     API.findPokemon(searchText).then(res => this.setState({pokemon: res.data}))
   }
-
-  
 
   render() {
     return (
@@ -34,7 +48,7 @@ class App extends Component {
           <p>Pokedex</p>
         </header>
         <SearchPokemon pokemon={this.state.pokemon} onSearch={this.handleSearchFunction} />
-        <PokemonInfo pokemon={this.state.pokemon} />
+        <PokemonInfo pokemon={this.state.pokemon} updateTeam={this.updateTeam}/>
       </div>
     );
   }
--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
+import '../css/PokemonInfo.css'
 
 class PokemonInfo extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      name: '',
-      sprites: [],
-      types: [],
-      stats: [],
-      moves: []
-    }
-  }
-
-  componentDidUpdate = () => {
-    const { pokemon } = this.props;
-    if (pokemon.name !== this.state.name)
-    this.setState({
-      name: pokemon.name,
-      sprites: pokemon.sprites,
-      types: pokemon.types,
-      stats: pokemon.stats,
-      moves: pokemon.moves
-    })
+  addToTeam = () => {
+    this.props.updateTeam();
   }
 
   render() {
+    const { pokemon } = this.props;
     return(
       <div className="pokemon-info">
-        {/* <div className="pokemon-pic"><img alt="pokemon" src={require(this.props.sprite)} /> </div> */}
-        <div className="pokemon-details">
-        Name: {this.state.name}
-        Type: {this.state.types.map(type => type.type.name)}
+      { pokemon.name ?  
+        <div className="container">
+          <div className="pokemon-pic">
+            <img alt="pokemon" src={pokemon.sprites.front_default} /> 
+          </div>
+          <div className="pokemon-details">
+            Name: {pokemon.name} <br />
+            <ul className="type">
+              Types: {pokemon.types.map(type => (
+                <li key={type.type.name}> {type.type.name} </li>
+              ))} 
+            </ul>
+            <ul className="stats"> 
+              Stats: {pokemon.stats.map(stat => (
+                <li key={stat.stat.name}> {stat.stat.name}: {stat.base_stat} </li>
+              ))}
+            </ul>
+            <ul className="moves">
+              Moves: {pokemon.moves.slice(0, 5).map(move => (
+                <li key={move.move.name}> {move.move.name} </li>
+              ))}
+            </ul>    
+          </div>      
+          <button type="button" onClick={this.addToTeam} >Add To Team</button>  
         </div>
+        : 
+        <h1>No Pokemon Selected</h1>
+      }
       </div>
     )
   }
