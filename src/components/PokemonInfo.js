a3f34d2db6757c12ebaceb62223e7fecbd9879ab
Devon Marantz <DMarantz@talentpath.com>
2019-03-20 07:44:45 -0700

    Make Pokemon details box

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -13,27 +13,34 @@ class PokemonInfo extends Component {
       <div className="pokemon-info">
       { pokemon.name ?  
         <div className="container">
-          <div className="pokemon-pic">
-            <img alt="pokemon" src={pokemon.sprites.front_default} /> 
-          </div>
-          <div className="pokemon-details">
-            <div className="pokemon-name"> {pokemon.name} </div>
-              <div className="pokemon-type">TYPE {pokemon.types.map(type => (
-                <div className="type" key={type.type.name}> {type.type.name} </div>
-              ))}
-              </div> 
-            <ul className="stats"> 
-              STATS {pokemon.stats.map(stat => (
-                <li key={stat.stat.name}> {stat.stat.name}: {stat.base_stat} </li>
-              ))}
-            </ul>
-            <ul className="moves">
-              MOVES {pokemon.moves.slice(0, 5).map(move => (
-                <li key={move.move.name}> {move.move.name} </li>
-              ))}
-            </ul>    
-          </div>      
-          <button type="button" onClick={this.addToTeam} >Add To Team</button>  
+          <div class="row">
+            <div class="column">
+              <div className="pokemon-pic">
+                <img alt="pokemon" src={pokemon.sprites.front_default} /> 
+              </div>
+            </div>
+            <div class="column">
+              <div className="pokemon-details">
+                <div className="pokemon-name"> {pokemon.name} </div>
+                <div className="pokemon-type">TYPE {pokemon.types.map(type => (
+                  <div className="type" key={type.type.name}> {type.type.name} </div>
+                ))}
+                </div>
+                <div className="pokemon-height">HEIGHT {pokemon.height}</div> 
+                <div className="pokemon-weight">WEIGHT {pokemon.weight}</div> 
+                
+                <ul className="stats"> 
+                  STATS {pokemon.stats.map(stat => (
+                    <li key={stat.stat.name}> {stat.stat.name}: {stat.base_stat} </li>
+                    ))}
+                </ul>
+              </div>      
+            </div>
+        </div>
+          <div className="details-footer">
+            <button type="button" onClick={this.addToTeam} >View Moves</button> 
+            <button type="button" onClick={this.addToTeam} >Add To Team</button> 
+          </div> 
         </div>
         : 
         <h1>No Pokemon Selected</h1>
