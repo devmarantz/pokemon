a828e14060f171cf65d8bc680a3c2b79220d4cc8
Ivy <yunjilim@gmail.com>
2019-03-25 16:26:21 -0700

    working on team-moves

--- src/components/MyTeam.js
+++ src/components/MyTeam.js
@@ -4,10 +4,9 @@ import '../css/MyTeam.css';
 class MyTeam extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       isHidden: false,
-      selected: {},
+      selected: 0,
     }
   }
 
@@ -20,14 +19,23 @@ class MyTeam extends Component {
   }
 
   selectPokemon = index => {
-    const selected = this.props.myTeam.filter((teamPokemon, idx) => {
-      return index === idx;
-    })
-    console.log(selected);
-    // this.setState({selected})
+    // const selected = this.props.myTeam.filter((teamPokemon, idx) => {
+    //   return index === idx;
+    // })
+    this.setState({ selected: index });
   }
 
-  editMoves = pokemon => {
+  editMoves = idx => {
+    this.setState({ 
+      selected: idx,
+    });
+    
+  }
+
+  removeSelectedMove = (e) => {
+    e.preventDefault();
+    // find selected pokemon
+    // edit remove move
 
   }
 
@@ -39,11 +47,18 @@ class MyTeam extends Component {
         <div className="team-pokemon-list">
         {(myTeam.length > 0) ? myTeam.map((pokemon, idx) => 
           <div className="team-pokemon" key={pokemon.id} onClick={() => this.selectPokemon(idx)} >
-            { this.state.isHidden && <div><button className="x-pokemon" onClick={() => this.removePokemon(idx)} >X</button> <button className="edit-moves-button" onClick={() => this.editMoves(pokemon)} >Edit Moves</button></div> }
+            { this.state.isHidden && <div><button className="x-pokemon" onClick={() => this.removePokemon(idx)} >X</button> <button className="edit-moves-button" onClick={() => this.editMoves(idx)} >Edit Moves</button></div> }
             <img alt="pokemon" src={ pokemon.sprites.front_default }  /><br />
-            { pokemon.name } 
+            { pokemon.name } <br />
+            {/* Moves: {pokemon.selectedMoves.map((move, idx) => (
+            <div key={move.move.name}>
+              {move.move.name}
+            </div>))} */}
           </div>
         ) : <div> No Pokemon on your Team </div> }
+        </div>
+        <div className='team-details'>
+          
         </div>
           <button className="edit-team" onClick={this.editTeam} >{ this.state.isHidden ? "Close" : "Edit Team" }</button>
       </div>
